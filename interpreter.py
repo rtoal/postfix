258b0be9dcf38c23acc38f6d742708e8e9612244
Ray Toal <rtoal@lmu.edu>
2016-07-02 17:48:23 -0700

    Simplify internal program representation

--- interpreter.py
+++ interpreter.py
@@ -6,62 +6,58 @@
 import operator
 
 class Command(object):
-    def __init__(self, kind, argument=None):
-        self.kind = kind
-        self.argument = argument
-
-    def __repr__(self):
-        return self.kind + (' ' + str(self.argument) if self.argument else '')
+    def __init__(self, value):
+        self.value = value
 
     def execute(self, commands, stack):
-        if self.kind in ('STRING', 'INT', 'SEQUENCE'):
-            stack.append(self.argument)
-        elif self.kind in ['le', 'lt', 'eq', 'ne', 'ge', 'gt']:
+        if isinstance(self.value, int) or isinstance(self.value, list):
+            stack.append(self.value)
+        elif self.value in ['le', 'lt', 'eq', 'ne', 'ge', 'gt']:
             _check_two_integers_on_top(stack)
             y = stack.pop()
             x = stack.pop()
-            stack.append(1 if operator.__dict__[self.kind](x, y) else 0)
-        elif self.kind in ['add', 'sub', 'mul', 'div', 'rem']:
+            stack.append(1 if operator.__dict__[self.value](x, y) else 0)
+        elif self.value in ['add', 'sub', 'mul', 'div', 'rem']:
             _check_two_integers_on_top(stack)
             y = stack.pop()
             x = stack.pop()
-            op = 'mod' if self.kind == 'rem' else self.kind
+            op = 'mod' if self.value == 'rem' else self.value
             _check_not_dividing_by_zero(x, y)
             stack.append(operator.__dict__[op](x, y))
-        elif self.kind == 'pop':
+        elif self.value == 'pop':
             _check_minimum_length(stack, 1)
             stack.pop()
-        elif self.kind == 'swap':
+        elif self.value == 'swap':
             _check_minimum_length(stack, 2)
             stack[-1], stack[-2] = stack[-2], stack[-1]
-        elif self.kind == 'sel':
+        elif self.value == 'sel':
             _check_minimum_length(stack, 3)
             _check_integer_on_top(stack)
             condition = stack.pop()
             y = stack.pop()
             x = stack.pop()
             stack.append(x if condition else y)
-        elif self.kind == 'get':
+        elif self.value == 'get':
             _check_integer_on_top(stack)
             index = stack.pop()
             _check_minimum_length(stack, index)
             stack.append(stack[~index])
-        elif self.kind == 'put':
+        elif self.value == 'put':
             _check_minimum_length(stack, 2)
             _check_integer_on_top(stack)
             index = stack.pop()
             value = stack.pop()
             _check_minimum_length(stack, index)
             stack[~index] = value
-        elif self.kind == 'prs':
+        elif self.value == 'prs':
             _check_string_on_top(stack)
-            print stack.pop()
-        elif self.kind == 'pri':
+            print(stack.pop())
+        elif self.value == 'pri':
             _check_integer_on_top(stack)
-            print stack.pop()
-        elif self.kind == 'exec':
+            print(stack.pop())
+        elif self.value == 'exec':
             _check_command_list_on_top(stack)
-            commands.extend(self.argument)
+            commands.extend(self.value)
 
 
 def _check(condition, message):
@@ -69,40 +65,39 @@ def _check(condition, message):
         raise(Exception, message)
 
 def _check_minimum_length(stack, expected_length):
-    _check(len(stack) >= expected_length, 'Expected stack to have %d items' % expected_length)
+    _check(len(stack) >= expected_length, 'Expected stack to have {} items'.format(expected_length))
 
 def _check_two_integers_on_top(stack):
     _check_minimum_length(stack, 2)
     _check_integer_on_top(stack)
-    _check(type(stack[-2]) in (int, long), 'Expected integer just below top of stack')
+    _check(isinstance(stack[-2], int), 'Expected integer just below top of stack')
 
 def _check_integer_on_top(stack):
     _check_minimum_length(stack, 1)
-    _check(type(stack[-1]) in (int, long), 'Expected integer on top of stack')
+    _check(isinstance(stack[1], int), 'Expected integer on top of stack')
 
 def _check_string_on_top(stack):
     _check_minimum_length(stack, 1)
-    _check(type(stack[-1]) in (str, unicode), 'Expected string on top of stack')
+    _check(isinstance(stack[-1], str), 'Expected string on top of stack')
 
 def _check_command_list_on_top(stack):
     _check_minimum_length(stack, 1)
-    _check(stack[-1].isinstance(Command) and stack[-1].kind == 'SEQUENCE',
-        'Expected command list on top of stack')
+    _check(isinstance(stack[-1], list), 'Expected command list on top of stack')
 
-def _check_not_dividing_by_zero(kind, divisor):
-    _check(kind not in ('div','rem') or divisor != 0, 'Divide by zero error')
+def _check_not_dividing_by_zero(command, divisor):
+    _check(command not in ('div','rem') or divisor != 0, 'Divide by zero error')
 
 
 def run(commands, stack, debug=False):
     commands = commands[:]
     while commands:
         if debug:
-            print 'Commands:', commands
-            print 'Stack', stack
-            print '--------'
+            print('Commands:', commands)
+            print('Stack', stack)
+            print('--------')
         command = commands.pop(0)
         command.execute(commands, stack)
     program_result = stack[-1] if stack else None
     if debug:
-        print 'Returning', stack[-1]
+        print('Returning', stack[-1])
     return program_result
