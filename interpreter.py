12cf99dd0163a477a2b4c95d4da511e7e3f6af74
Ray Toal <rtoal@lmu.edu>
2016-07-03 19:44:59 -0700

    Clean up validators

--- interpreter.py
+++ interpreter.py
@@ -6,13 +6,14 @@ import operator
 def execute(command, commands, stack):
     if isinstance(command, int) or isinstance(command, list):
         stack.append(command)
-    elif command in ['le', 'lt', 'eq', 'ne', 'ge', 'gt', 'add', 'sub', 'mul', 'div', 'rem']:
-        _check_integer(stack, 1)
-        _check_integer(stack, 2)
+    elif command in 'le lt eq ne ge gt add sub mul div rem'.split():
+        _check_minimum_length(stack, 2)
+        _check_integer(stack[-1])
+        _check_integer(stack[-2])
         y = stack.pop()
         x = stack.pop()
         if command in ('div', 'rem'):
-            _check_not_dividing_by_zero(command, y)
+            _check_non_zero(y)
             command = {'div': 'floordiv', 'rem': 'mod'}[command]
         stack.append(int(operator.__dict__[command](x, y)))
     elif command == 'pop':
@@ -22,29 +23,24 @@ def execute(command, commands, stack):
         _check_minimum_length(stack, 2)
         stack[-1], stack[-2] = stack[-2], stack[-1]
     elif command == 'sel':
-        _check_integer(stack, 3)
+        _check_minimum_length(stack, 3)
+        _check_integer(stack[-3])
         x = stack.pop()
         y = stack.pop()
         condition = stack.pop()
         stack.append(y if condition else x)
     elif command == 'nget':
-        _check_integer(stack)
+        _check_minimum_length(stack, 1)
+        _check_integer(stack[-1])
         index = stack.pop()
         _check_positive_integer(index)
         _check_minimum_length(stack, index)
-        _check_integer(stack, index)           # Not allowed to access a non-int
+        _check_integer(stack[-index])           # Not allowed to access a non-int
         stack.append(stack[-index])
-    elif command == 'put':
-        _check_minimum_length(stack, 2)
-        _check_integer(stack)
-        index = stack.pop()
-        value = stack.pop()
-        _check_minimum_length(stack, index)
-        stack[~index] = value
     elif command == 'exec':
-        _check_list_on_top(stack)
-        new_commands = stack.pop()
-        commands[:0] = new_commands
+        _check_minimum_length(stack, 1)
+        _check_list(stack[-1])
+        commands[:0] = stack.pop()
 
 
 def _check(condition, message):
@@ -54,20 +50,17 @@ def _check(condition, message):
 def _check_minimum_length(stack, expected_length):
     _check(len(stack) >= expected_length, 'STACK_SIZE_UNDER_{}'.format(expected_length))
 
-def _check_integer(stack, pos=1):
-    _check_minimum_length(stack, pos)
-    _check(isinstance(stack[-pos], int), 'INT_EXPECTED')
+def _check_integer(value):
+    _check(isinstance(value, int), 'INT_EXPECTED')
 
-def _check_positive_integer(n):
-    _check(isinstance(n, int) and n >= 1, 'POSITIVE_INT_EXPECTED')
+def _check_non_zero(value):
+    _check(value != 0, 'DIVIDE_BY_ZERO')
 
-def _check_list_on_top(stack):
-    _check_minimum_length(stack, 1)
-    _check(isinstance(stack[-1], list), 'LIST_EXPECTED')
+def _check_positive_integer(value):
+    _check(isinstance(value, int) and value >= 1, 'POSITIVE_INT_EXPECTED')
 
-def _check_not_dividing_by_zero(command, divisor):
-    _check(command not in ('div','rem') or divisor != 0, 'DIVIDE_BY_ZERO')
-
+def _check_list(value):
+    _check(isinstance(value, list), 'LIST_EXPECTED')
 
 def run(commands, *stack):
     try:
